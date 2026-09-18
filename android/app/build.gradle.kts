ee58c9f255152da08cc7e3287adbcff9bf6cf0cd
yafu <zhyaphoo@qq.com>
2026-09-18 19:15:47 +0800

    v1.0.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 13
-        versionName = "2.3"
+        versionName = "1.0.0"
 
         // 只打真机需要的 ABI：iroh 的 Rust QUIC 库单 ABI 就有 9~15MB，
         // 全 ABI 打包会让 APK 白背 3 份。需要模拟器调试时临时加回 x86_64。
